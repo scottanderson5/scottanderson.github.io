1758c84957a072aaad92ec04dae6706953fbce90
Scott <scottanderson5@gmail.com>
2016-05-25 14:30:32 -0500

    Gallery Popup modals added to Research Section

--- _site/JQuery/main.js
+++ _site/JQuery/main.js
@@ -1,42 +1,23 @@
-
+//Pop Up Modals
 
 $(document).ready(function() {	
-	$('.test-popup-link').magnificPopup({
+	$('.gameonlg-popup-link').magnificPopup({
 	  type: 'image'
 	  // other options
 	});
 });	
 
 
+$(document).ready(function() {		
+	$('.gallery-itemA').magnificPopup({
+  		type: 'image',
+  		gallery:{
+    		enabled:true
+  		}
+	});
+});
 
 
 
 
-//$(document).ready(function() {
-//   if($(".splash").is(":visible")) {
-//        $(".wrapper").css({"opacity":"0"});
-//    }
 
-//    $(".splash-arrow").click(function() {
-//        $(".splash").slideUp("800", function() {
-//            $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
-//        });
-//    });
-//});
-
-//$(window).scroll(function() {
-//    $(window).off("scroll");
-//    $(".splash").slideUp("800", function() {
-//        $("html, body").animate({"scrollTop":"0px"},100);
-//        $(".wrapper").delay(100).animate({"opacity":"1.0"},800);
-//    });
-//});
-
-//PRELOADER
-    //<![CDATA[
-       // $(window).load(function() { // makes sure the whole site is loaded
-         //   $('.status').fadeOut(); // will first fade out the loading animation
-         //   $('.preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-         //   $('body').delay(350).css({'overflow':'visible'});
-       // })
-    //]]>
